d26cef4f781aa88a893bd9c948dfb8fa804b35bd
jonathandion <jonathandionalary@gmail.com>
2022-12-30 22:35:41 -0500

    main - testing my nix flake config

--- src/init.lua
+++ src/init.lua
@@ -7,8 +7,6 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   vim.cmd([[packadd packer.nvim]])
 end
 
--- hello world
-
 -- @plugins
 require("packer").startup(function(use)
   use("wbthomason/packer.nvim")
