560edebcd3a517b19be18bfb91c114136c333160
jonathandion <jonathandionalary@gmail.com>
2023-01-26 23:41:32 -0500

    main - added lockfile and tweaks

--- src/init.lua
+++ src/init.lua
@@ -1,6 +1,6 @@
 -- @variables
 local user_cmd = vim.api.nvim_create_user_command
-local keyset = vim.keymap.set
+local map = vim.keymap.set
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 -- space as leader key
@@ -47,6 +47,7 @@ require("lazy").setup({
   },
   -- Text manipulation plugins
   { "tpope/vim-surround", event = "VeryLazy" },
+  { "tpope/vim-repeat", event = "VeryLazy" },
   {
     "echasnovski/mini.comment",
     version = '*',
@@ -106,19 +107,19 @@ require("lazy").setup({
   {
     "nvim-telescope/telescope.nvim",
     keys = {
-      { "<C-f>", ":Telescope find_files<cr>", desc = "Find files"  },
+      { "<C-f>", ":Telescope find_files<cr>", desc = "Find files" },
       { "<C-p>", ":Telescope commands<cr>", desc = "Commands" },
       { "<C-e>", ":Telescope grep_string<cr>", desc = "Grep String" },
       { "<leader>fb", ":Telescope buffers<cr>", desc = "[F]ind [B]uffers" },
-      { "<leader>fd", ":Telescope diagnostics<cr>",  desc = "[F]ind [D]iagnostics" },
-      { "<leader>ff", ":Telescope git_status<cr>",  desc = "[F]ind [F]iles Git Status" },
-      { "<leader>fh", ":Telescope oldfiles<cr>",  desc = "[F]ind [H]istory" },
-      { "<leader>fg", ":Telescope live_grep<cr>",  desc = "[F]ind [G]rep" },
+      { "<leader>fd", ":Telescope diagnostics<cr>", desc = "[F]ind [D]iagnostics" },
+      { "<leader>ff", ":Telescope git_status<cr>", desc = "[F]ind [F]iles Git Status" },
+      { "<leader>fh", ":Telescope oldfiles<cr>", desc = "[F]ind [H]istory" },
+      { "<leader>fg", ":Telescope live_grep<cr>", desc = "[F]ind [G]rep" },
       { "<leader>fr", ":Telescope registers<cr>", desc = "[Find] [R]egisters" },
       { "<leader>fk", ":Telescope keymaps<cr>", desc = "[Find] [K]eymaps" },
-      { "<leader>fm", ":Telescope marks<cr>",  desc = "[Find] [M]arks" },
+      { "<leader>fm", ":Telescope marks<cr>", desc = "[Find] [M]arks" },
     },
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    dependencies = { { "nvim-lua/plenary.nvim", lazy = true } },
     config = function()
       require("telescope").setup({
         defaults = {
@@ -160,11 +161,13 @@ require("lazy").setup({
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
-          "lua",
-          "bash",
-          "json",
+          "html",
           "javascript",
           "typescript",
+          "json",
+          "lua",
+          "bash",
+          "markdown",
         },
         sync_install = false,
         auto_install = true,
@@ -192,8 +195,6 @@ require("lazy").setup({
       { "hrsh7th/cmp-path", event = "VeryLazy" },
       { "hrsh7th/cmp-nvim-lsp", event = "VeryLazy" },
       { "hrsh7th/cmp-nvim-lua", event = "VeryLazy" },
-      -- Snippets
-      { "rafamadriz/friendly-snippets", event = "VeryLazy" },
       -- UI
       { "j-hui/fidget.nvim", config = true, event = "VeryLazy" },
     },
@@ -216,6 +217,7 @@ require("lazy").setup({
         "stylelint_lsp",
         "eslint",
         "marksman",
+        "astro",
 
         -- web3/blockchain
         "solang",
@@ -244,14 +246,14 @@ require("lazy").setup({
       })
 
       lsp.on_attach(function(_, bufnr)
-        keyset("n", "gI", vim.lsp.buf.implementation, { desc = "[G]oto [I]mplementation" })
-        keyset("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
-        keyset("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
-        keyset("n", "<leader>f", vim.lsp.buf.format, { desc = "[F]ormat current buffer" })
+        map("n", "gI", vim.lsp.buf.implementation, { desc = "[G]oto [I]mplementation" })
+        map("n", "<leader>rn", vim.lsp.buf.rename, { desc = "[R]e[n]ame" })
+        map("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+        map("n", "<leader>f", vim.lsp.buf.format, { desc = "[F]ormat current buffer" })
 
         local telescope = require("telescope.builtin")
-        keyset("n", "<leader>ds", telescope.lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
-        keyset("n", "<leader>ws", telescope.lsp_dynamic_workspace_symbols, { desc = "[W]orkspace [S]ymbols" })
+        map("n", "<leader>ds", telescope.lsp_document_symbols, { desc = "[D]ocument [S]ymbols" })
+        map("n", "<leader>ws", telescope.lsp_dynamic_workspace_symbols, { desc = "[W]orkspace [S]ymbols" })
 
         -- Create a command `:Format` local to the LSP buffer
         vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
@@ -270,40 +272,47 @@ require("lazy").setup({
 })
 
 -- @options
+local opt = vim.opt
+opt.path = table.concat({ "**" }) -- Search in subdirectories
+opt.background = "dark" -- Dark background
+opt.backup = false -- Disable backup
+opt.breakindent = true -- Wrap text with indent
+opt.clipboard = "unnamedplus" -- Sync with system clipboard
+opt.cursorline = true -- Enable highlighting of the current line
+opt.expandtab = true -- Use spaces instead of tabs
+opt.guicursor = "" -- Fat cursor (old vim style)
+opt.history = 500 -- History size
+opt.hlsearch = true -- Highlight search
+opt.ignorecase = true -- Ignore case
+opt.incsearch = true -- Show search matches
+opt.laststatus = 0 -- No status line
+opt.magic = true -- Regular expressions
+opt.shiftwidth = 2 -- Size of an indent
+opt.showmatch = true -- Show matching brackets
+opt.signcolumn = "yes" -- Always show the signcolumn, otherwise it would shift the text each time
+opt.smartcase = true -- Don't ignore case with capitals
+opt.smarttab = true -- Insert indents automatically
+opt.smartindent = true -- Insert indents automatically
+opt.so = 7 -- Minimum number of screen lines to keep above and below the cursor
+opt.softtabstop = 0 -- Number of spaces tabs count for
+opt.timeoutlen = 500 -- Time to wait for a mapped sequence to complete (in milliseconds)
+opt.title = true -- Set the terminal title
+opt.ttimeout = true -- Enable timeout for key mappings
+opt.ttimeoutlen = 10 -- Time to wait for a mapped sequence to complete (in milliseconds)
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true -- Enable persistent undo
+opt.updatetime = 250 -- Save swap file and trigger CursorHold
+opt.wildmenu = true -- Enable command-line completion mode
+opt.wildmode = "full" -- Command-line completion mode
+opt.wrap = true -- Wrap long lines
+opt.writebackup = false -- Disable backup
+opt.spelllang = { "en" } -- Spell check english
+opt.grepprg = "rg --vimgrep" -- Use ripgrep for grepping
+opt.tabstop = 2 -- Number of spaces tabs count for
+opt.termguicolors = true -- True color support
+opt.iskeyword:append("-") -- Treat dash separated words as a word text object
+
 vim.wo.number = true
-vim.o.path = table.concat({ "**" })
-vim.o.background = "dark"
-vim.o.backup = false
-vim.o.breakindent = true
-vim.o.clipboard = "unnamedplus"
-vim.o.cursorline = true
-vim.o.expandtab = true
-vim.o.guicursor = "" -- fat cursor
-vim.o.history = 500
-vim.o.hlsearch = true
-vim.o.ignorecase = true
-vim.o.incsearch = true
-vim.o.laststatus = 2
-vim.o.magic = true
-vim.o.shiftwidth = 2
-vim.o.showmatch = true
-vim.o.signcolumn = "yes"
-vim.o.smartcase = true
-vim.o.smarttab = true
-vim.o.so = 7
-vim.o.softtabstop = 0
-vim.o.tabstop = 2
-vim.o.timeoutlen = 500
-vim.o.title = true
-vim.o.ttimeout = true
-vim.o.ttimeoutlen = 10
-vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.o.undofile = true
-vim.o.updatetime = 250
-vim.o.wildmenu = true
-vim.o.wildmode = "full"
-vim.o.wrap = true
-vim.o.writebackup = false
 
 -- Netrw config (file explorer)
 vim.g.netrw_banner = 1
@@ -324,17 +333,7 @@ vim.filetype.add({
   },
 })
 
-function _G.abbreviate_or_noop(input, output)
-  local cmdtype = vim.fn.getcmdtype()
-  local cmdline = vim.fn.getcmdline()
-
-  if cmdtype == ":" and cmdline == input then
-    return output
-  else
-    return input
-  end
-end
-
+-- @auto-commands
 -- Run Eslint automatically on certain filetypes
 vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   pattern = "*.{js,ts,jsx,tsx}",
@@ -347,7 +346,25 @@ vim.api.nvim_create_autocmd({ "BufWritePost" }, {
   end,
 })
 
+-- Highlight on yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
 -- @commands
+function _G.abbreviate_or_noop(input, output)
+  local cmdtype = vim.fn.getcmdtype()
+  local cmdline = vim.fn.getcmdline()
+
+  if cmdtype == ":" and cmdline == input then
+    return output
+  else
+    return input
+  end
+end
+
 function _G.cmd_alias(alias, cmd)
   vim.api.nvim_command(
     "cabbrev <expr> " .. alias .. " " .. "v:lua.abbreviate_or_noop('" .. alias .. "', '" .. cmd .. "')"
@@ -359,29 +376,31 @@ cmd_alias("tsnode", "! ts-node %")
 
 -- @keymaps
 -- black hole
-keyset("n", "<leader>dd", '"_dd')
+map("n", "<leader>dd", '"_dd')
 -- remove highlight
-keyset("n", "<Esc><Esc>", ":noh<cr>")
+map("n", "<Esc><Esc>", ":noh<cr>")
 -- navigate fast between paragraphs
-keyset("n", "<down>", "}")
-keyset("n", "<up>", "{")
+map("n", "<down>", "}")
+map("n", "<up>", "{")
 -- quit
-keyset("n", "<leader>q", ":bd<cr>")
--- reload current file
-keyset("n", "<leader>r", ":e %<cr>")
+map("n", "<leader>q", ":bd<cr>")
+-- reload current buffer
+map("n", "<leader>r", ":e %<cr>")
+-- format buffer
+map("n", "<leader>f", ":Format<cr>")
 -- save
-keyset("n", "<leader>w", ":w<cr>")
+map("n", "<leader>w", ":w<cr>")
 -- fast search
-keyset("n", "<C-space>", "/")
+map("n", "<C-space>", "/")
 -- file explorer
-keyset("n", "<leader>-", ":Explore<cr>")
+map("n", "<leader>-", ":Explore<cr>")
 -- replace word under cursor
-keyset("n", "<C-n>", ":%s#<c-r><c-w>##g<left><left>")
-keyset("v", "<C-n>", ":s#<c-r><c-w>##g<left><left>")
+map("n", "<C-n>", ":%s#<c-r><c-w>##g<left><left>")
+map("v", "<C-n>", ":s#<c-r><c-w>##g<left><left>")
 -- diagnostic
-keyset("n", "<leader>d", vim.diagnostic.open_float)
-keyset("n", "[d", vim.diagnostic.goto_prev)
-keyset("n", "]d", vim.diagnostic.goto_next)
+map("n", "<leader>d", vim.diagnostic.open_float)
+map("n", "[d", vim.diagnostic.goto_prev)
+map("n", "]d", vim.diagnostic.goto_next)
 
 -- copy current buffer absolute path into clipboard
 user_cmd("CopyCurrentPath", function()
@@ -389,4 +408,4 @@ user_cmd("CopyCurrentPath", function()
   vim.fn.setreg("+", path)
   vim.notify('Copied "' .. path .. '" to the clipboard!')
 end, {})
-keyset("n", "<leader>y", ":CopyCurrentPath<cr>")
+map("n", "<leader>y", ":CopyCurrentPath<cr>")
